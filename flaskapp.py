fee305cfba2972d5b80f7051c6d6787bd4690a29
Interplay <machinate@live.com>
2017-10-27 02:34:07 -0400

    add delay for aws to create user and styling

--- flaskapp.py
+++ flaskapp.py
@@ -10,11 +10,11 @@ def hello():
 @app.route("/echo", methods=['POST'])
 def echo():
 	createuser=subprocess.Popen('aws iam create-user --user-name ' + str(request.form['text']), shell=True)
-	time.sleep(10)
+	time.sleep(4)
 	wait=subprocess.Popen('aws iam user-exists --user-name ' +str(request.form['text']), shell=True)
 	addgroup=subprocess.Popen('aws iam add-user-to-group --user-name ' + str(request.form['text']) + ' --group-name workshopGrp', shell=True)
 	createpass=subprocess.Popen('aws iam create-login-profile --user-name ' + str(request.form['text']) + ' --password hackpsu', shell=True)
-	return str(request.form['text']) +  " created"
+	return "User " str(request.form['text']) +  ' has been created. Your password is: hackpsu \n Log into the AWS environment at <a href="https://hackpsu.signin.aws.amazon.com/console"> https://hackpsu.signin.aws.amazon.com/console </a>'
 
 if __name__ == "__main__":
 	app.run(debug=True)
