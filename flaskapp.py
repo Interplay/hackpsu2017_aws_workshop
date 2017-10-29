9f4ddbb7079d8c3194023f0001edbd37dad69eb3
Interplay <machinate@live.com>
2017-10-29 01:44:55 -0400

    restructured directories to fit flask requirements and edit html

--- flaskapp.py
+++ flaskapp.py
@@ -1,11 +1,11 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 import subprocess, time
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-	return '<form action="/echo" method="POST"><input name="text"><input type="submit" value="Echo"></form>'
+	return render_template("index.html", jquerycdn='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js', bootstrapjs='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js', bootstrapcss='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-beta/css/bootstrap.min.css')
 
 @app.route("/echo", methods=['POST'])
 def echo():
@@ -14,7 +14,12 @@ def echo():
 	wait=subprocess.Popen('aws iam user-exists --user-name ' +str(request.form['text']), shell=True)
 	addgroup=subprocess.Popen('aws iam add-user-to-group --user-name ' + str(request.form['text']) + ' --group-name workshopGrp', shell=True)
 	createpass=subprocess.Popen('aws iam create-login-profile --user-name ' + str(request.form['text']) + ' --password hackpsu', shell=True)
-	return "User " str(request.form['text']) +  ' has been created. Your password is: hackpsu \n Log into the AWS environment at <a href="https://hackpsu.signin.aws.amazon.com/console"> https://hackpsu.signin.aws.amazon.com/console </a>'
+	return "User " + str(request.form['text']) +  ' has been created. Your password is: hackpsu <br> Log into the AWS environment at <a href="https://hackpsu.signin.aws.amazon.com/console"> https://hackpsu.signin.aws.amazon.com/console </a>'
 
 if __name__ == "__main__":
 	app.run(debug=True)
+
+# To Do
+# find a way to get html of the pages looking nicer
+# see if i can add css to reposition elements
+# html head equivalents
