03888c89f5eb8e9b054b2e61b18a5fd8dc92bd3d
Interplay <machinate@live.com>
2017-11-01 01:19:58 -0400

    add code snippets for workshop, code prettifier library and js functions to copy code to clipboard

--- static/main.js
+++ static/main.js
@@ -1,3 +1,36 @@
 $(document).ready(function() {
     $('#checkin').modal();
 });
+
+$(document).ready(function() {
+    var btn = document.getElementById('index');
+    var clipboard = new Clipboard(btn);
+    clipboard.on('success', function(e) {
+        console.log(e);
+    });
+    clipboard.on('error', function(e) {
+        console.log(e);
+    }); 
+});
+
+$(document).ready(function() {
+    var btn = document.getElementById('s3policy');
+    var clipboard = new Clipboard(btn);
+    clipboard.on('success', function(e) {
+        console.log(e);
+    });
+    clipboard.on('error', function(e) {
+        console.log(e);
+    }); 
+});
+
+$(document).ready(function() {
+    var btn = document.getElementById('ec2policy');
+    var clipboard = new Clipboard(btn);
+    clipboard.on('success', function(e) {
+        console.log(e);
+    });
+    clipboard.on('error', function(e) {
+        console.log(e);
+    }); 
+});
