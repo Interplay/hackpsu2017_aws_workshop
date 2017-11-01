0baacb2b13e5007c20108e0e5cf4966498719808
Interplay <machinate@live.com>
2017-11-01 02:18:55 -0400

    renamed files around

--- flaskapp.py
+++ flaskapp.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-	return render_template("index.html")
+	return render_template("root.html")
 
 @app.route("/echo", methods=['POST'])
 def echo():
