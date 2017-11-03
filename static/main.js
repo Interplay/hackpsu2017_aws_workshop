eb5f858aa8882645d482bf90a466b2be1a340d29
Interplay <machinate@live.com>
2017-11-02 22:52:28 -0400

    pictures for each step

--- static/main.js
+++ static/main.js
@@ -1,5 +1,18 @@
 $(document).ready(function() {
-    $('#checkin').modal();
+    $('#checkin').modal({dismissible: true});
+    $('#s3st1').modal({dismissible: true});
+    $('#s3st2').modal({dismissible: true});
+    $('#s3st3').modal({dismissible: true});
+    $('#s3st4').modal({dismissible: true});
+    $('#ec2st1').modal({dismissible: true});
+    $('#ec2st2').modal({dismissible: true});
+    $('#ec2st3').modal({dismissible: true});
+    $('#ec2st4').modal({dismissible: true});
+    $('#ec2st5').modal({dismissible: true});
+    $('#ec2st6').modal({dismissible: true});
+    $('#ec2st7').modal({dismissible: true});
+    $('#ec2st8').modal({dismissible: true});
+    $('#ec2st9').modal({dismissible: true});
 });
 
 $(document).ready(function() {
