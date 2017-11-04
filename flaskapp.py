8d7b9cd9be349d92699f5f4d1d0dae27a7ccc91b
Interplay <machinate@live.com>
2017-11-04 01:15:11 -0400

    add confirmation page

--- flaskapp.py
+++ flaskapp.py
@@ -14,9 +14,9 @@ def echo():
 	wait=subprocess.Popen('aws iam user-exists --user-name ' +str(request.form['text']), shell=True)
 	addgroup=subprocess.Popen('aws iam add-user-to-group --user-name ' + str(request.form['text']) + ' --group-name workshopGrp', shell=True)
 	createpass=subprocess.Popen('aws iam create-login-profile --user-name ' + str(request.form['text']) + ' --password hackpsu', shell=True)
-	return "User " + str(request.form['text']) +  ' has been created. Your password is: hackpsu <br> Log into the AWS environment at <a href="https://hackpsu.signin.aws.amazon.com/console"> https://hackpsu.signin.aws.amazon.com/console </a>'
+	return render_template("confirmationpage.html", username=str(request.form['text']))
+	#return "User " + str(request.form['text']) +  ' has been created. Your password is: hackpsu <br> Log into the AWS environment at <a href="https://hackpsu.signin.aws.amazon.com/console"> https://hackpsu.signin.aws.amazon.com/console </a>'
 
-#render a confirmation message
 #add arns to iam policies
 
 if __name__ == "__main__":
